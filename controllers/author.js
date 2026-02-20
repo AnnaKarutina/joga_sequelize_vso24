e9f9d7515246f7286eabd185a405c26ff8c83979
Anna Karutina <anna.karutina@khk.ee>
2026-02-20 14:46:30 +0200

    Fix author data name

--- controllers/author.js
+++ controllers/author.js
@@ -2,10 +2,10 @@ const { Author, Article } = require('../models/');
 
 const getAuthorArticles = async (req, res) => {
   try {
-    const articles = await Author.findByPk(req.params.id, {
+    const author = await Author.findByPk(req.params.id, {
         include: Article
     });
-    res.status(200).json(articles);
+    res.status(200).json(author);
   } catch (err) {
     console.error('Error fetching articles:', err.message);
     res.status(500).json({ error: 'Internal Server Error' });
